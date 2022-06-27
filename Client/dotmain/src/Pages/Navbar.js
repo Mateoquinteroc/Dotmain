25c11e271be6c9b62f4f0227caf28d60e47fd140
Mateo Quintero C <mateoquintero05@gmail.com>
2022-06-26 21:24:20 -0500

    creacion de nuevos componentes

---
+++ Client/dotmain/src/Pages/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <li>
+                <a href='/'>About</a>
+            </li>
+            <li>
+                <a href='/'>Project</a>
+            </li>
+            <li>
+                <a href='/'>Contact</a>
+            </li>
+        </nav>
+   </div>
+  )
+}
+
+export default Navbar;
