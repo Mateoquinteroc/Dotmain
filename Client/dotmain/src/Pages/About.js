2c5843004182c0c0e01bc7ccb84a4b2d700bacaa
Mateo Quintero C <mateoquintero05@gmail.com>
2022-07-06 23:48:42 -0500

    adelantos home about

--- Client/dotmain/src/Pages/About.js
+++ Client/dotmain/src/Pages/About.js
@@ -3,7 +3,13 @@ import '../styles/About.scss';
 
 const About = forwardRef ((_,ref) => {
   return (
-    <div className="naranja" ref={ref}>About</div>
+   <div className="about-container" ref={ref}>
+    <div className="item-container">
+      <h1 className="title">Dotmain</h1>
+      <p className="paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <img src="https://www3.gobiernodecanarias.org/medusa/mediateca/ecoescuela/wp-content/uploads/sites/2/2013/11/02-Camara-de-video.png" />
+    </div>
+   </div>
   )
 })
 
